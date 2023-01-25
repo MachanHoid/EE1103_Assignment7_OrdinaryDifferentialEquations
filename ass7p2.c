1963cba09129744b78b03fb0fe7b89733d7582f9
= <=>
2023-01-25 11:32:17 +0530

    Problem 2(b) done

--- ass7p2.c
+++ ass7p2.c
@@ -9,9 +9,15 @@ double deriv(double x, double y){
     return -100000*y + 99999*exp(-x);
 }
 
+//implicit euler method
+void prob2_implicit_euler(double *x, double *y, double dx, double (*deriv_func)(double x, double y)){
+    *x += dx;
+    *y = (*y + 99999*dx*exp(- *x))/(1 + 100000*dx);
+}
+
 void main(){
     //Problem 2
-    //(a)
+    //(a) Explicit Euler
     double yi, xi, xf, dx;
     yi = 0;
     xi = 0;
@@ -27,11 +33,14 @@ void main(){
     FILE* fp_explicit_euler2 = fopen("p2a_2_soln.txt", "w");
     integrator(yi, xi, xf, dx, &euler, &deriv, fp_explicit_euler2);
 
-    //using h = 3 e-5 (unstable case)
-    dx = 0.00003;
+    //using h = 2.1 e-5 (unstable case)
+    dx = 0.000021;
     FILE* fp_explicit_euler3 = fopen("p2a_3_soln.txt", "w");
     integrator(yi, xi, xf, dx, &euler, &deriv, fp_explicit_euler3);
 
 
-    //(b)
+    //(b) Implicit Euler
+    dx = 0.1;
+    FILE* fp_implicit_euler = fopen("p2b_soln.txt", "w");
+    integrator(yi, xi, xf, dx, &prob2_implicit_euler, &deriv, fp_implicit_euler);
 }
